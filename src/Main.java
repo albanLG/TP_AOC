27ca308285912152c5f6920f974cee28fafa9249
Le Goff Alban <alban.le-goff@etudiant.univ-rennes1.fr>
2021-10-21 09:16:08 +0200

    push

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
 	static Scheduler scheduler=new Scheduler();
+	static Capteur capteur=new CapteurImpl();
+	static List<ObserverDeCapteur> observers=new ArrayList<ObserverDeCapteur>();
 	static Long timeDelay=2L;
 	
 	public static void main(String[] args) {
